e0a41bc7960c667d8b8a90be207e6db250a390fb
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-07-03 10:21:36 +0300

    Update index.tsx
    Added new buttons

--- app/screens/signup/index.tsx
+++ app/screens/signup/index.tsx
@@ -1,7 +1,6 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Pressable, Image } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Link, Stack } from "expo-router";
 
 export default function SignUpIndex() {
   return (
@@ -14,11 +13,49 @@ export default function SignUpIndex() {
       </Text>
       <TextInput
         className="text-[17px] mt-1 py-2 caret-black font-normal leading-[22px] tracking-[-0.43px] border-b-[1px] border-b-[#D1D1D1]"
-        // style={{ height: 40 }}
         placeholder="Name"
         // onChangeText={(newText) => setText(newText)}
         // defaultValue={text}
       />
+      <View className="items-center">
+        <Link href="./phoneNumber" className="mt-[56px]" asChild>
+          <Pressable className="bg-[#636363] w-[361px] h-[52px] items-center justify-center rounded-[99px]">
+            <Text className="text-white text-[17px] font-[600] leading-[22px] tracking-[-0.43px]">
+              Continue
+            </Text>
+          </Pressable>
+        </Link>
+        <Text className="text-[#242424] py-5 text-[17px] font-[600] leading-[22px] tracking-[-0.43px]">
+          Or
+        </Text>
+        <Pressable className="space-x-3 border border-[#636363] flex-row w-[361px] h-[52px] items-center justify-center rounded-[99px]">
+          <Image
+            className="h-5 w-5"
+            source={require("../../../assets/images/googleLogo.png")}
+          />
+          <Text className="text-[#636363] text-[17px] font-[600] leading-[22px] tracking-[-0.43px]">
+            Continue with Google
+          </Text>
+        </Pressable>
+        <Pressable className="space-x-3 border border-[#636363] flex-row w-[361px] h-[52px] mt-4  items-center justify-center rounded-[99px]">
+          <Image
+            className="h-5 w-5"
+            source={require("../../../assets/images/appleLogo.png")}
+          />
+          <Text className="text-[#636363] text-[17px] font-[600] leading-[22px] tracking-[-0.43px]">
+            Continue with Apple
+          </Text>
+        </Pressable>
+        <Pressable className="space-x-3 flex-row w-[361px] h-[52px] mt-4  items-center justify-center rounded-[99px]">
+          <Image
+            className="h-[15px] w-[15px]"
+            source={require("../../../assets/images/searchLogo.png")}
+          />
+          <Text className="text-[#636363] text-[17px] font-[600] leading-[22px] tracking-[-0.43px]">
+            Find my account
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
