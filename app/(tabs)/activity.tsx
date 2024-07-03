b1141699b0db9da905e311f4cf9734a8e18cfdc6
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-07-03 21:37:12 +0300

    Update activity.tsx

--- app/(tabs)/activity.tsx
+++ app/(tabs)/activity.tsx
@@ -1,13 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector } from "react-redux"; // Import useSelector
+import { RootState } from "../store"; // Adjust the path
 
 export default function Activity() {
+  // Retrieve the name from the store
+  const name = useSelector((state: RootState) => state.userData.name);
   return (
-    <SafeAreaView className="justify-center items-center bg-[#ffd700] flex-1">
-      <Text className="text-xl font-semibold text-[#32cd32] uppercase">
+    <SafeAreaView className="justify-center items-center bg-[#32cd32] h-full">
+      <Text className="text-xl font-semibold text-[#ffd700] uppercase">
         Activity
       </Text>
+      <Text className="text-xl font-semibold text-[#ffd700]">{name}</Text>
     </SafeAreaView>
   );
 }
