4aaab2d2669b8399b7e32781b6e228b31908daaa
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-07-06 00:11:11 +0300

    Update activity.tsx

--- app/(tabs)/activity.tsx
+++ app/(tabs)/activity.tsx
@@ -6,13 +6,18 @@ import { RootState } from "../store"; // Adjust the path
 
 export default function Activity() {
   // Retrieve the name from the store
-  const name = useSelector((state: RootState) => state.userData.name);
+  const userData = useSelector((state: RootState) => state.userData);
   return (
     <SafeAreaView className="justify-center items-center bg-[#32cd32] h-full">
       <Text className="text-xl font-semibold text-[#ffd700] uppercase">
         Activity
       </Text>
-      <Text className="text-xl font-semibold text-[#ffd700]">{name}</Text>
+      <Text className="text-xl font-semibold text-[#ffd700]">
+        Welcome {userData.name}
+      </Text>
+      <Text className="text-xl font-semibold text-[#ffd700] mt-2">
+        Your Phone Number is: {userData.phoneNumber}
+      </Text>
     </SafeAreaView>
   );
 }
