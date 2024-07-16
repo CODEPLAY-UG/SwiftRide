859b4415b1ef6291936eda21e491279e0a541168
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-07-17 01:05:50 +0300

    feat: setting up the drawer

--- app/screens/home/index.tsx
+++ app/screens/home/index.tsx
@@ -2,14 +2,22 @@ import { View, Text } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux"; // Import useSelector
 import { RootState } from "../../store"; // Adjust the path
+import { Stack } from "expo-router";
 
 export default function Index() {
   // Retrieve the name from the store
   const name = useSelector((state: RootState) => state.userData.name);
 
   return (
-    <View className="h-full justify-center items-center bg-[#32cd32]">
-      <Text className="text-xl font-semibold text-[#ffd700]">{name}</Text>
+    <View className="flex-1 justify-center items-center bg-[#32cd32]">
+      {/* <Stack.Screen
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          // headerTitleAlign: "center",
+        }}
+      /> */}
+      <Text className="text-xl font-semibold text-[#ffd700]">Here we go</Text>
     </View>
   );
 }
