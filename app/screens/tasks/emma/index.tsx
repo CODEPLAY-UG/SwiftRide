5225ec277c362b72f18c09d050750c5fba09e881
wanyike <wanyikeemmanuel@gmail.com>
2024-07-24 13:44:15 +0300

    update index

--- app/screens/tasks/emma/index.tsx
+++ app/screens/tasks/emma/index.tsx
@@ -1,85 +1,107 @@
-import { View, Text, TextInput, Pressable, Image, TouchableOpacity, GestureResponderEvent } from "react-native";
-import React, { useState } from "react";
-import { Link, router, Stack, Tabs } from "expo-router";
+import { View, Text, Image, Pressable } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack, router } from "expo-router";
+import { ChevronRight, SunMoon } from "lucide-react-native";
 
-import { ArrowLeft, BellIcon, ChevronRight, Lock, Phone, SunMoon } from "lucide-react-native";
-import { Button } from "react-native-paper";
-import { Switch } from "react-native-paper";
-
-export default function settings() {
-  function handleSettings(text: string): void {}
-  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
-
-  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
-
+export default function Profile() {
   return (
-    <View className="bg-white h-full w-full px-5">
-      <Stack.Screen options={{ title: "Profile", headerTitleAlign: "center" }} />
-      <View className="space-y-5 mt-[12px]">
-        <View className="flex-row justify-between items-center">
-          <View className="flex-row items-center">
-            <BellIcon color="#808080" size={20} />
-            <View className="mx-4">
-              <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Notifications</Text>
-              <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">This allows us to send you promos, {"\n"} deals among things</Text>
+    <SafeAreaView className=" bg-white h-full w-full px-6 flex-1">
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerTitle: "Profile",
+        }}
+      />
+
+      <View className=" flex h-[136px] gap-x-[2px]">
+        <View className="justify-center items-center">
+          <Image className="h-[72px] w-[72px]" source={require(".../../../assets/images/profil.png")} />
+        </View>
+        <Pressable>
+          <View className="flex flex-row  items-center p-4 gap-x-[21px]">
+            <View>
+              <Text className="text-[17px] font-normal leading-[22px] text-[#242424]">Name</Text>
+              <Text className="text-[13px] font-normal leading-[18px] text-[#616161]">Phone number/email</Text>
             </View>
           </View>
-
-          <View className="">
-            {/* <Switch /> */}
-            <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
-          </View>
-        </View>
-        <View className="flex-row justify-between items-center">
-          <View className="flex-row items-center">
-            <Lock color="#808080" size={20} />
-            <View className="mx-4">
-              <Text className=" text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Privacy</Text>
-              <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">Manage the data you share with us</Text>
-            </View>
-          </View>
-
-          <View className="">
-            <ChevronRight color="#808080" size={20} />
-          </View>
-        </View>
-
-        <View className="flex-row justify-between items-center ">
-          <View className="flex-row items-center">
-            <SunMoon color="#808080" size={20} />
-            <View className="mx-4">
-              <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Appearance</Text>
-              <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">Select how you want your app to look like</Text>
-            </View>
-          </View>
-
-          <View className="">
-            <ChevronRight color="#808080" size={20} />
-          </View>
-        </View>
-
-        <View className="flex-row justify-between items-center  ">
-          <View className="flex-row items-center">
-            <Phone color="#808080" size={20} />
-            <View className="mx-4">
-              <Text className="text-[#242424] h-[22px] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Communication</Text>
-              <Text className=" text-[#616161] h-[18px] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">Choose your preferred contact methods</Text>
-            </View>
-          </View>
-
-          <View className="">
-            <ChevronRight color="#808080" size={20} />
-          </View>
-        </View>
+        </Pressable>
       </View>
 
-      <View className="justify-center gap-5 items-center mt-40 mb-2">
-        <Link href="./login&security" className="mt-5 mb-2" asChild>
-          <Pressable>
-            <Text className="text-[#426380] text-lg">Next Page</Text>
-          </Pressable>
-        </Link>
+      <View className="flex flex-col gap-y-[24px] h-[336px] mt-[20px]">
+        <View className="flex flex-row gap-x-[2px]  ">
+          <SunMoon color="#808080" size={20} />
+          <View className="flex flex-row gap-x-[12px]  ">
+            <Text className=" w-[280px] text-[17px] font-normal leading-[22px] text-[#242424] p-[1px] ">Settings</Text>
+          </View>
+          <ChevronRight color="#808080" size={20} />
+        </View>
+
+        <View className="flex flex-row gap-x-[2px] ">
+          <SunMoon color="#808080" size={20} />
+          <View className="flex flex-row gap-x-[12px]">
+            <Text className="w-[280px] text-[17px] font-normal leading-[22px] text-[#242424] p-[1px] ">Login & security</Text>
+          </View>
+          <ChevronRight color="#808080" size={20} />
+        </View>
+
+        <View className="flex flex-row gap-x-[2px]  ">
+          <SunMoon color="#808080" size={20} />
+          <View className="flex flex-row gap-x-[12px]  ">
+            <Text className=" w-[280px] text-[17px] font-normal leading-[22px] text-[#242424] p-[1px] ">Visit help centre</Text>
+          </View>
+          <View className="">
+            <ChevronRight color="#808080" size={20} />
+          </View>
+        </View>
+
+        <View className="flex flex-row gap-x-[2px] ">
+          <View className="flex flex-row gap-x-[12px]">
+            <Text className="w-[280px]  text-[17px] font-normal leading-[22px] text-[#242424] p-[1px] ">Help</Text>
+          </View>
+          <View className="">
+            <ChevronRight color="#808080" size={20} />
+          </View>
+        </View>
+        <View className="flex flex-row gap-x-[2px]">
+          <View className="flex flex-row gap-x-[12px]">
+            <Text className="w-[280px] text-[17px] font-normal leading-[22px] text-[#242424] p-[1px] ">Give us feedback</Text>
+          </View>
+          <View className="">
+            <ChevronRight color="#808080" size={20} />
+          </View>
+        </View>
+
+        <View className="flex flex-row gap-x-[2px] ">
+          <View className="flex flex-row gap-x-[12px]">
+            <Text className="w-[280px] text-[17px] font-normal leading-[22px] text-[#242424] p-[1px]">Safety & support</Text>
+          </View>
+          <View className="">
+            <ChevronRight color="#808080" size={20} />
+          </View>
+        </View>
+
+        <View className="flex flex-row gap-x-[2px] ">
+          <View className="flex flex-row gap-x-[12px]">
+            <Text className="w-[280px] text-[17px] font-normal leading-[22px] text-[#242424] p-[1px] ">Legal</Text>
+          </View>
+          <View className="">
+            <ChevronRight color="#808080" size={20} />
+          </View>
+        </View>
+
+        <View className="flex flex-row gap-x-[2px] ">
+          <View className="flex flex-row gap-x-[12px]">
+            <Text className="w-[280px] text-[17px] font-normal leading-[22px] text-[#242424] p-[1px]">About</Text>
+          </View>
+          <View className="">
+            <ChevronRight color="#808080" size={20} />
+          </View>
+        </View>
+        <Pressable className="items-center justify-center ">
+          <Text className="  text-[#C50F1F] font-[400]  text-[17px] leading-[22px]">Log out</Text>
+        </Pressable>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
