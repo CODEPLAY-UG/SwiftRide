52586b3ab8289a1109a7d81573cc69b28d0d3857
wanyike <wanyikeemmanuel@gmail.com>
2024-07-26 19:15:33 +0300

    Update privacy.tsx

--- app/screens/tasks/Emma/privacy.tsx
+++ app/screens/tasks/Emma/privacy.tsx
@@ -2,7 +2,7 @@ import { View, Text, TextInput, Pressable, Image, TouchableOpacity, GestureRespo
 import React, { useState } from "react";
 import { Link, router, Stack, Tabs } from "expo-router";
 
-import { ArrowLeft, BellIcon, ChevronRight, Lock, MapPinIcon, Phone, ShapesIcon, SparklesIcon, SunMoon, WalletIcon } from "lucide-react-native";
+import { ArrowLeft, BellIcon, ChevronRight, Lock, MapPinIcon, Phone, RadioIcon, ShapesIcon, SparklesIcon, SunMoon, WalletIcon } from "lucide-react-native";
 import { Button } from "react-native-paper";
 import { Switch } from "react-native-paper";
 
@@ -15,7 +15,7 @@ export default function Privacy() {
   return (
     <View className="bg-white h-full w-full px-5">
       <Stack.Screen options={{ title: "Privacy centre", headerTitleAlign: "center" }} />
-      <View className="space-y-5 mt-[12px]">
+      <View className="space-y-8 mt-[12px]">
         <View className="flex-row justify-between items-center">
           <View className="flex-row items-center">
             <ShapesIcon color="#808080" size={24} />
@@ -35,6 +35,19 @@ export default function Privacy() {
             <View className="mx-4">
               <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Device location</Text>
               <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">Check if we can access your location</Text>
+            </View>
+          </View>
+          <View className="">
+            <ChevronRight color="#808080" size={20} />
+          </View>
+        </View>
+
+        <View className="flex-row justify-between items-center ">
+          <View className="flex-row items-center">
+            <RadioIcon color="#808080" size={24} />
+            <View className="mx-4">
+              <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Live location</Text>
+              <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">How we share your location in real-time{"\n"}withothers</Text>
             </View>
           </View>
           <View className="">
