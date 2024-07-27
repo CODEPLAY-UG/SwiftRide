03bda0ed1121377629a4f9d885a8205c969d8327
reagan luyinda <luyindareagan@gmail.com>
2024-07-27 04:37:01 +0300

    feat: Added new checkbox
    added a new checkbox to the emergency contacts screen

--- app/screens/tasks/reagan/emergencyindex.tsx
+++ app/screens/tasks/reagan/emergencyindex.tsx
@@ -5,7 +5,9 @@ import { Search } from "lucide-react-native";
 import Checkbox from "expo-checkbox";
 
 export default function EmergencyContacts() {
-  const [isChecked, setChecked] = useState(false);
+  const [isChecked1, setChecked1] = useState(false);
+  const [isChecked2, setChecked2] = useState(false);
+  
   return (
     <View className="bg-white h-full w-full">
       <Stack.Screen
@@ -25,11 +27,11 @@ export default function EmergencyContacts() {
       <Text className="text-center p-[16px]">Select Contact</Text>
       <View className="flex-row items-center mx-10 py-5 space-x-4">
         <Checkbox
-          // style={styles.checkbox}
+          
           className="rounded-full"
-          value={isChecked}
-          onValueChange={setChecked}
-          color={isChecked ? "#808080" : undefined}
+          value={isChecked1}
+          onValueChange={setChecked1}
+          color={isChecked1 ? "#808080" : undefined}
         />
         <Image
           className="w-10 h-10 rounded-full"
@@ -44,6 +46,30 @@ export default function EmergencyContacts() {
           </Text>
         </View>
       </View>
+      
+     <View className="flex-row items-center mx-10 py-5 space-x-4">
+        <Checkbox
+         
+          className="rounded-full"
+          value={isChecked2}
+          onValueChange={setChecked2}
+          color={isChecked2 ? "#808080" : undefined}
+        />
+        <Image
+          className="w-10 h-10 rounded-full"
+          source={require("@assets/images/pink.png")}
+        />
+        <View className="">
+          <Text className="text-[17px] text-[#242424] leading-[22px] tracking-[-0.43]">
+            Luyinda Reagan
+          </Text>
+          <Text className="text-[13px] text-[#808080] leading-[18px] tracking-[-0.08]">
+            +256 755565084
+          </Text>
+        </View>
+      </View>
+
+
       <View className="items-center">
         <TouchableOpacity className="bg-[#636363] h-[52px] w-[361px] rounded-full justify-center items-center">
           <Text className="text-[#FFFFFF]">Add Contact</Text>
