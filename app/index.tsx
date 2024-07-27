fcf20a547c9eaf0b1019674f2b6c0f0ec0bc15c4
wanyike <wanyikeemmanuel@gmail.com>
2024-07-27 12:36:31 +0300

    created login screens

--- app/index.tsx
+++ app/index.tsx
@@ -16,18 +16,20 @@ export default function RootIndex() {
           headerTitleAlign: "center",
         }}
       />
-      <Text className="uppercase font-bold text-lg text-[#ffd700]">
-        Welcome to expo
-      </Text>
+      <Text className="uppercase font-bold text-lg text-[#ffd700]">Welcome to expo</Text>
       <Link href="/screens/signup" className="mt-5 mb-2" asChild>
         <Pressable>
           <Text className="text-[#32cd32] text-lg">Go to sign up</Text>
         </Pressable>
       </Link>
-      <TouchableOpacity
-        onPress={handleLocation}
-        className="mt-3 bg-[#00ffff] py-2 px-4 rounded"
-      >
+
+      <Link href="/screens/login" className="mt-5 mb-2" asChild>
+        <Pressable className="bg-[#0011ff]  py-2 px-4 rounded">
+          <Text className="text-[#cd326b] text-lg">Go to Login</Text>
+        </Pressable>
+      </Link>
+
+      <TouchableOpacity onPress={handleLocation} className="mt-3 bg-[#00ffff] py-2 px-4 rounded">
         <Text className="text-[#3a1a6c] text-lg">Go to tasks</Text>
       </TouchableOpacity>
       {/* <Redirect href="/screens/signup" /> */}
