73d7d493fec7841d59cd6f16f907990378220959
Lubega Leonald <lubegaleo2@gmail.com>
2024-07-31 07:49:21 +0300

    payment mode updated

--- components/core/PaymentMode.tsx
+++ components/core/PaymentMode.tsx
@@ -1,15 +1,25 @@
 import { View, Text, Pressable } from "react-native";
-import React from "react";
-import { Stack } from "expo-router";
+import React, { useState } from "react";
+import { router, Stack } from "expo-router";
 import { Banknote, Circle, CircleCheck, Info, Plus } from "lucide-react-native";
+import { RadioButton } from "react-native-paper";
 
-export default function TripSummary() {
+export default function PaymentMode() {
+  const [checked, setChecked] = useState("four");
   return (
     <View className="px-5 h-[80%] justify-between">
       <View className="">
-        <View className="flex-row space-x-5 justify-between item-center mt-5">
+        <View className="flex-row space-x-5 justify-between item-center mt-3">
           <View className="p-1 flex-row items-center space-x-2">
-            <Circle color="#808080" size={24} />
+            <RadioButton
+              value=""
+              status={checked == "one" ? "checked" : "unchecked"}
+              onPress={() => {
+                setChecked("one");
+              }}
+              color="#636363"
+              uncheckedColor="#636363"
+            />
             <View className="">
               <Text className="text-[17px] text-[#242424] leading-[22px] tracking-[-0.43px]">
                 Wallet
@@ -19,16 +29,25 @@ export default function TripSummary() {
               </Text>
             </View>
           </View>
-          <Pressable>
-            <Text className="flex-row items-center justify-center border rounded-[99px] h-fit px-2 py-[5px]">
-              <Plus color="#107C10" size={16} /> Deposit
-            </Text>
+          <Pressable
+            onPress={() => router.push("../")}
+            className="flex-row items-center justify-center bg-[#107C10] rounded-[99px] h-8 px-3 space-x-1">
+            <Plus color="#ffffff" size={16} />
+            <Text className="text-[#ffffff]">Deposit</Text>
           </Pressable>
         </View>
 
-        <View className="flex-row space-x-5 justify-between item-center mt-5">
+        <View className="flex-row space-x-5 justify-between item-center mt-3">
           <View className="p-1 flex-row items-center space-x-2">
-            <Circle className="" color="#808080" size={24} />
+            <RadioButton
+              value=""
+              status={checked == "two" ? "checked" : "unchecked"}
+              onPress={() => {
+                setChecked("two");
+              }}
+              color="#636363"
+              uncheckedColor="#636363"
+            />
             <View className="">
               <Text className="text-[17px] text-[#242424] leading-[22px] tracking-[-0.43px]">
                 Pay with Airtel money
@@ -37,9 +56,17 @@ export default function TripSummary() {
           </View>
         </View>
 
-        <View className="flex-row space-x-5 justify-between item-center mt-5">
+        <View className="flex-row space-x-5 justify-between item-center mt-3">
           <View className="p-1 flex-row items-center space-x-2">
-            <Circle className="" color="#808080" size={24} />
+            <RadioButton
+              value=""
+              status={checked == "three" ? "checked" : "unchecked"}
+              onPress={() => {
+                setChecked("three");
+              }}
+              color="#636363"
+              uncheckedColor="#636363"
+            />
             <View className="">
               <Text className="text-[17px] text-[#242424] leading-[22px] tracking-[-0.43px]">
                 Pay with MTN momo
@@ -48,9 +75,17 @@ export default function TripSummary() {
           </View>
         </View>
 
-        <View className="flex-row space-x-5 justify-between item-center mt-5">
+        <View className="flex-row space-x-5 justify-between item-center mt-3">
           <View className="p-1 flex-row items-center space-x-2">
-            <CircleCheck className="" color="#808080" size={24} />
+            <RadioButton
+              value=""
+              status={checked == "four" ? "checked" : "unchecked"}
+              onPress={() => {
+                setChecked("four");
+              }}
+              uncheckedColor="#636363"
+              color="#636363"
+            />
             <View className="">
               <Text className="text-[17px] text-[#242424] leading-[22px] tracking-[-0.43px]">
                 Pay with cash
