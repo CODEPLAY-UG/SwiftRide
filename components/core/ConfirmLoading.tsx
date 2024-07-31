b63b0c48775e139cb9617ffe8013928b198b691e
Lubega Leonald <lubegaleo2@gmail.com>
2024-07-31 17:15:03 +0300

    loading driver

---
+++ components/core/ConfirmLoading.tsx
@@ -0,0 +1,45 @@
+import { View, Text, Pressable } from "react-native";
+import React, { useState } from "react";
+import { router, Stack } from "expo-router";
+import { Banknote, Info, Star } from "lucide-react-native";
+import {
+  ActivityIndicator,
+  Badge,
+  Chip,
+  ProgressBar,
+} from "react-native-paper";
+
+export default function ConfirmLoading(props: {
+  setIsPaymentDetailsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const [status, isStatus] = useState(false);
+
+  const changeStatus = () => {
+    isStatus((prev) => !prev);
+    props.setIsPaymentDetailsOpen(status);
+  };
+
+  return (
+    <View className="px-5 h-[80%] justify-between">
+      <View className="px-4 mt-2">
+        <View className="flex-row space-x-5 justify-between item-center">
+          <View className="flex-row items-center space-x-2 mt-3">
+            <View className="">
+              <Text className="text-[17px] text-[#242424] leading-[22px] tracking-[-0.43px]">
+                Connecting to your driver
+              </Text>
+              <Text className="text-[15px] text-[#616161] leading-[20px] tracking-[-0.23px]">
+                We're getting your driver ready. You'll be on your way in no
+                time.
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View className="flex-row space-x-5 justify-center item-center mt-8">
+          <ActivityIndicator animating={true} color="#636363" size="large" />
+        </View>
+      </View>
+    </View>
+  );
+}
