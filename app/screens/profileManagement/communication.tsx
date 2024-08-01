d0b09c5e9e2681fbdb391e9c361ecdb64d08cc33
wanyike <wanyikeemmanuel@gmail.com>
2024-08-01 17:32:49 +0300

    created screen communication

--- app/screens/profileManagement/communication.tsx
+++ app/screens/profileManagement/communication.tsx
@@ -2,11 +2,16 @@ import { View, Text, TextInput, Pressable, Image, TouchableOpacity, GestureRespo
 import React, { useState } from "react";
 import { Link, router, Stack, Tabs } from "expo-router";
 
-import { ArrowLeft, BellIcon, ChevronRight, CircleCheckIcon, DotIcon, Info, Lock, Phone, SparklesIcon, SunMoon, WalletIcon } from "lucide-react-native";
-import { Button } from "react-native-paper";
+import { ArrowLeft, BellIcon, BellRingIcon, ChevronRight, CircleCheckIcon, DotIcon, Info, Lock, Phone, SparklesIcon, SunMoon, WalletIcon } from "lucide-react-native";
+import { Button, Checkbox, RadioButton, Switch, DefaultTheme } from "react-native-paper";
+import { setStatusBarHidden } from "expo-status-bar";
 
 export default function Communication() {
   function handlePaying(text: string): void {}
+
+  const [value, setValue] = React.useState("first");
+
+  const [checked, setChecked] = React.useState("first");
 
   return (
     <View className="bg-white h-full w-full px-4">
@@ -38,16 +43,48 @@ export default function Communication() {
           </View>
         </View>
 
-        <View className="space-y-3">
+        <View className="space-y-4 px-4">
+          <View className="flex-row justify-between items-center">
+            <View className="flex-row items-center">
+              <RadioButton value="first" status={checked === "first" ? "checked" : "unchecked"} onPress={() => setChecked("first")} color="#6200ee" uncheckedColor="#ffffff" />
+              <View className="mx-4">
+                <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Calls</Text>
+              </View>
+            </View>
+            <View className="bg-[#F1FAF1] py-[2] px-[8] rounded-[4px]">
+              <Text className="text-[#0E700E] text-[15px] font-normal leading-[20px] tracking-[-0.23]">Recommended</Text>
+            </View>
+          </View>
+          <View className="flex-row justify-between items-center">
+            <View className="flex-row items-center">
+              <RadioButton value="second" status={checked === "second" ? "checked" : "unchecked"} onPress={() => setChecked("second")} color="#6200ee" uncheckedColor="#ffffff" />
+              <View className="mx-4">
+                <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-[-0.43]">Chat</Text>
+              </View>
+            </View>
+          </View>
+
+          <View className="flex-row justify-between items-center">
+            <View className="flex-row items-center">
+              <RadioButton value="third" status={checked === "third" ? "checked" : "unchecked"} onPress={() => setChecked("third")} color="#6200ee" uncheckedColor="#ffffff" />
+              <View className="mx-4">
+                <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-[-0.43]">Call or chat</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <View className="space-y-6">
           <View className="flex-row justify-between items-center">
             <View className="px-4">
               <Text className="text-[#242424] text-[17px] font-semibold leading-[22px] tracking-[-0.43]">Push notifications</Text>
             </View>
           </View>
+
           <View className="flex-row justify-between items-center px-4">
             <View className="flex-row items-center ">
-              <View className=" ">
-                <Image className="h-[24px] w-[24] rounded-[100px]" source={require(".../../../assets/images/checked.png")} />
+              <View className="">
+                <Checkbox status={checked ? "checked" : "unchecked"} color="#636363" onPress={() => {}} />
               </View>
               <View className="mx-4">
                 <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Recommendations</Text>
@@ -59,7 +96,9 @@ export default function Communication() {
           <View className="flex-row justify-between items-center px-4">
             <View className="flex-row items-center ">
               <View className=" ">
-                <Image className="h-[24px] w-[24] rounded-[100px]" source={require(".../../../assets/images/checked.png")} />
+                <View className="">
+                  <Checkbox status={checked ? "checked" : "unchecked"} color="#636363" onPress={() => {}} />
+                </View>
               </View>
               <View className="mx-4">
                 <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Promotional offers</Text>
@@ -70,7 +109,9 @@ export default function Communication() {
           <View className="flex-row justify-between items-center px-4">
             <View className="flex-row items-center ">
               <View className=" ">
-                <Image className="h-[24px] w-[24] rounded-[100px]" source={require(".../../../assets/images/checked.png")} />
+                <View className="">
+                  <Checkbox status={checked ? "checked" : "unchecked"} color="#636363" onPress={() => {}} />
+                </View>
               </View>
               <View className="mx-4">
                 <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Product news & updates</Text>
