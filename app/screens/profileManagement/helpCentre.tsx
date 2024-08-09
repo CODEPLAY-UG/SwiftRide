3ad98a4ed647ee7cfe1b3f961f3422c7057aaea8
wanyike <wanyikeemmanuel@gmail.com>
2024-08-09 12:26:10 +0300

    update help centre
    changed Icons

--- app/screens/profileManagement/helpCentre.tsx
+++ app/screens/profileManagement/helpCentre.tsx
@@ -4,46 +4,53 @@ import { Link, router, Stack, Tabs } from "expo-router";
 
 import { ArrowLeft, BellIcon, ChevronRight, Lock, Phone, SparklesIcon, SunMoon, WalletIcon } from "lucide-react-native";
 import { Button } from "react-native-paper";
+import ArrowIcon from "@/assets/svgs/arrowIcon";
+import SparkIcon from "@/assets/svgs/sparkIcon";
+import WaltIcon from "@/assets/svgs/waltIcon";
 
 export default function Helpcentre() {
   function handleHelpcentre(text: string): void {}
 
   return (
-    <View className="bg-white h-full w-full px-5">
+    <View className="bg-white h-full w-full">
       <Stack.Screen
         options={{
           headerShown: true,
           title: "Help centre",
           headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontSize: 17, // Adjust the font size
+            fontWeight: "semibold", // Adjust the font weight
+          },
         }}
       />
-      <View className="space-y-5 mt-[12px]">
+      <View className="">
         <TouchableOpacity onPress={() => router.push("./gettingStarted")} className="">
-          <View className="flex-row justify-between items-center">
-            <View className="flex-row items-center">
-              <SparklesIcon color="#808080" size={24} />
+          <View className="gap-[24px] mt-1 justify-between flex-row items-center px-5">
+            <View className="flex-row -mx-3 items-center">
+              <SparkIcon />
               <View className="mx-4">
                 <Text className=" text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Getting started with SwiftRide</Text>
                 <Text className=" text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">An introduction to how the app works</Text>
               </View>
             </View>
             <View className="">
-              <ChevronRight color="#808080" size={20} />
+              <ArrowIcon />
             </View>
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => router.push("./payingTrip")} className="">
-          <View className="flex-row justify-between items-center ">
-            <View className="flex-row items-center">
-              <WalletIcon color="#808080" size={24} />
+          <View className="gap-[24px] mt-2 justify-between flex-row items-center px-5">
+            <View className="flex-row -mx-3 items-center">
+              <WaltIcon />
               <View className="mx-4">
                 <Text className="text-[#242424] text-[17px] font-normal leading-[22px] tracking-tighter[-0.43]">Paying for your trip</Text>
                 <Text className="text-[#616161] text-[13px] font-normal leading-[18px] tracking-tighter[-0.08]">Find out how we price our rides</Text>
               </View>
             </View>
             <View className="">
-              <ChevronRight color="#808080" size={20} />
+              <ArrowIcon />
             </View>
           </View>
         </TouchableOpacity>
