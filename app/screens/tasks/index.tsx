12f94f0426e5521d14d055511605596dbd793f94
wanyike <wanyikeemmanuel@gmail.com>
2024-08-13 11:37:09 +0300

    linked emma
    linked profile Management

--- app/screens/tasks/index.tsx
+++ app/screens/tasks/index.tsx
@@ -6,7 +6,7 @@ export default function TaskIndex() {
   return (
     <View className="h-full justify-center gap-5 items-center">
       <Stack.Screen options={{ title: "Tasks", headerTitleAlign: "center" }} />
-      <TouchableOpacity onPress={() => router.push("./Emma")} className="">
+      <TouchableOpacity onPress={() => router.push("/screens/profileManagement")} className="">
         <Text className="bg-[#0754ba] text-[#ffd700] text-xl px-5 py-1 rounded-md border-2 border-[#ffd700]">
           Emma
         </Text>
