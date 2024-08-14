451859bcfa3eb38bc5dde2bd07bfc1e92b49946f
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-08-14 14:17:16 +0300

    refactor: Improved on the ui layout

--- app/screens/onBoarding/index.tsx
+++ app/screens/onBoarding/index.tsx
@@ -1,13 +1,15 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
 import React from "react";
-import { ShieldPlus, Smile, TrafficCone } from "lucide-react-native";
 import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { LinearGradient } from "expo-linear-gradient";
+import SheildPlus from "@/assets/svgs/sheildPlus";
+import Smile from "@/assets/svgs/smile";
+import TrafficCone from "@/assets/svgs/trafficCone";
 
 export default function onboarding() {
   return (
-    <View className="w-full h-full">
+    <View className="w-full h-full ">
       <StatusBar style="light" />
 
       <Stack.Screen options={{ headerShown: false }} />
@@ -19,48 +21,55 @@ export default function onboarding() {
         <LinearGradient
           colors={[
             "rgba(0,0,0,0)",
-            "rgba(0,0,0,0.5)",
-            "rgba(0,0,0,0.8)",
+            "rgba(0,0,0,0.6)",
             "rgba(0,0,0,0.9)",
+            "rgba(0,0,0,1)",
           ]}
           style={{
             position: "absolute",
             left: 0,
             right: 0,
             bottom: 0,
-            height: "50%",
+            height: "30%",
           }}
         />
-        <View className="my-10 z-10">
-          <View>
-            <Text className="text-white text-[34px] font-[700] text-center leading-[41px] tracking-[0.4px]">
-              {"Ride in greater\ncomfort"}
-            </Text>
-            <View className="flex-row justify-center space-x-16 p-8">
-              <View className="items-center">
-                <ShieldPlus size={24} color="white" />
+        <View className="flex-col py-9 items-start self-stretch px-4 ">
+          <View className="items-center self-stretch px-4">
+            <View className="w-full mt-8">
+              <Text className="text-white text-[34px] font-[700] text-center leading-[41px] tracking-[0.4px]">
+                {"Ride in greater\ncomfort"}
+              </Text>
+            </View>
+            <View className="flex-row justify-center space-x-8">
+              <View className="items-center py-2 w-[80px]">
+                <View className="py-2">
+                  <SheildPlus width={32} height={32} color="white" />
+                </View>
                 <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
                   {"Safer\nrides"}
                 </Text>
               </View>
-              <View className="items-center">
-                <Smile size={24} color="white" />
+              <View className="items-center py-2 w-[80px]">
+                <View className="py-2">
+                  <Smile width={32} height={32} color="white" />
+                </View>
                 <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
                   {"More bike\noptions"}
                 </Text>
               </View>
-              <View className="items-center justify-center">
-                <TrafficCone size={24} color="white" />
+              <View className="items-center py-2 w-[80px]">
+                <View className="py-2">
+                  <TrafficCone width={32} height={32} color="white" />
+                </View>
                 <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
                   {"Better\nnavigation"}
                 </Text>
               </View>
             </View>
           </View>
-
-          <View className="px-3 space-y-1">
+          <View className=" w-full pt-2 space-y-1">
             <Pressable
-              onPress={() => router.replace("./login_phoneNumber")}
+              onPress={() => router.push("./login_phoneNumber")}
               className="bg-[#636363] w-full h-[52px] items-center justify-center rounded-[99px]"
             >
               <Text className="text-white text-[17px] font-[600] leading-[22px] tracking-[-0.43px]">
