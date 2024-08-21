16954887c2939406a31fdcc9a37a25e76102c392
Lubega Leonald <lubegaleo2@gmail.com>
2024-08-21 10:56:11 +0300

    upadte search1

--- components/core/search1.tsx
+++ components/core/search1.tsx
@@ -4,13 +4,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Mic, Search, X } from "lucide-react-native";
 import { Stack } from "expo-router";
 
-interface LocationSuggestion {
-  name: string;
-  place_formatted: string;
-  distance: number;
-  // Add other properties as needed
-}
-
 import getMapSearches from "@services/mapSearches";
 
 export default function SearchComponent({
@@ -20,7 +13,7 @@ export default function SearchComponent({
 }: {
   placeholder?: string;
   setIsTyping: Dispatch<SetStateAction<boolean>>;
-  setLocationSuggestions: Dispatch<SetStateAction<LocationSuggestion[]>>;
+  setLocationSuggestions: Dispatch<SetStateAction<string[]>>;
 }) {
   const [text, setText] = useState("");
   const [isFocused, setIsFocused] = useState(false);
