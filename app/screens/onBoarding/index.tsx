ca05d26adfa978200e68cfeee8fbfb5765d47002
Tibamwenda Anthony <anthonyngisiro@gmail.com>
2024-11-12 03:05:40 +0300

    Align the assets components

--- app/screens/onBoarding/index.tsx
+++ app/screens/onBoarding/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
 import React from "react";
-import { ShieldPlus, Smile, TrafficCone } from "lucide-react-native";
+// import { ShieldPlus, Smile, TrafficCone } from "lucide-react-native";
 import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { LinearGradient } from "expo-linear-gradient";
 import Button from "@/components/navigation/Button";
+import SheildPlus from "@/assets/svgs/sheildPlus";
+import Smile from "@/assets/svgs/smile";
+import TrafficCone from "@/assets/svgs/trafficCone";
 
 export default function onBoarding() {
   return (
@@ -33,33 +36,33 @@ export default function onBoarding() {
           }}
         />
         <View className="my-10 z-10">
-          <View>
-            <Text className="text-white text-[34px] font-[700] text-center leading-[41px] tracking-[0.4px]">
-              {"Ride in greater\ncomfort"}
-            </Text>
-            <View className="flex-row justify-center space-x-16 p-8">
-              <View className="items-center">
-                <ShieldPlus size={24} color="white" />
-                <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
-                  {"Safer\nrides"}
-                </Text>
+          <View className="flex-row justify-center space-x-8 ">
+            <View className="items-center py-2 w-[80px]">
+              <View className="py-2">
+                <SheildPlus width={32} height={32} color="white" />
               </View>
-              <View className="items-center">
-                <Smile size={24} color="white" />
-                <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
-                  {"More bike\noptions"}
-                </Text>
+              <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
+                {"Safer\nrides"}
+              </Text>
+            </View>
+            <View className="items-center py-2 w-[80px]">
+              <View className="py-2">
+                <Smile width={32} height={32} color="white" />
               </View>
-              <View className="items-center justify-center">
-                <TrafficCone size={24} color="white" />
-                <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
-                  {"Better\nnavigation"}
-                </Text>
+              <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
+                {"More bike\noptions"}
+              </Text>
+            </View>
+            <View className="items-center py-2 w-[80px]">
+              <View className="py-2">
+                <TrafficCone width={32} height={32} color="white" />
               </View>
+              <Text className="text-white text-[13px] text-center leading-[18px] tracking-[-0.08px]">
+                {"Better\nnavigation"}
+              </Text>
             </View>
           </View>
-
-          <View className="px-3 space-y-3">
+          <View className="px-3 space-y-3 mt-2">
             <Button
               title="Log in"
               onPress={() =>
